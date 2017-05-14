65779d611971224b464feacc963fff6d6fd0c48f
remew <foolishremew@gmail.com>
2017-05-14 12:58:08 +0900

    separate test

--- test/test-accessible-exists-key.js
+++ test/test-accessible-exists-key.js
@@ -13,6 +13,9 @@ test('accessible exists key', t => {
     const obj = NullSafeObject.create(srcObj);
     t.is(obj.key, 'value');
     t.is(obj.deep.property.access, true);
+});
+test('accessible not exists key in deep', t => {
+    const obj = NullSafeObject.create(srcObj);
     t.truthy(obj.deep.other_property.access);
 });
 
